cb698f532b0e3701ac7287d949b88152bb57c55a
ryodgie <ryodgie@gmail.com>
2023-03-15 16:11:55 +0800

    feat:  complete

---
+++ README.md
@@ -0,0 +1,50 @@
+# 🍦 mint chocolate chip profile card component 😋
+
+![Design preview for the QR code component coding challenge](./src/assets/preview.png)
+
+## Layout
+
+The designs were created to the following widths:
+
+- Desktop: 1440px
+- Mobile: 375px
+
+## Links
+
+- Solution URL: [https://github.com/eigdoyr/profile-card](https://github.com/eigdoyr/product-preview-card)
+- Live Site URL: [https://eigdoyr-profile-card.netlify.com](https://eigdoyr-product-preview-card.netlify.com)
+
+## Built with
+
+- [Vue](https://vuejs.org/) - JS library
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="bg-pattern-top"></div>
+  <div class="bg-pattern-bottom"></div>
+  <main>
+    <div class="card">
+      <div class="card-banner">
+        <img
+          class="img-avatar"
+          src="../src/assets/image-victor.jpg"
+          alt="user profile pic"
+        />
+      </div>
+
+      <div class="user-info">
+        <h1 class="user-name">Victor Crest <span class="user-age">26</span></h1>
+        <h2 class="user-location">London</h2>
+      </div>
+      <div class="stats">
+        <ul class="user-stats">
+          <li class="stats-followers">
+            <ul>
+              <li class="stats-count">80K</li>
+              <li class="stats-label">Followers</li>
+            </ul>
+          </li>
+          <li class="stats-likes">
+            <ul>
+              <li class="stats-count">803K</li>
+              <li class="stats-label">Likes</li>
+            </ul>
+          </li>
+          <li class="stats-photos">
+            <ul>
+              <li class="stats-count">1.4K</li>
+              <li class="stats-label">Photos</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: -ms-grid;
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+}
+
+.card {
+  text-align: center;
+  -webkit-box-shadow: 3px 3px 15px hsla(0, 0%, 0%, 0.218);
+  box-shadow: 3px 3px 15px hsla(0, 0%, 0%, 0.218);
+  border-radius: 1em;
+  min-width: 320px;
+}
+
+.card-banner {
+  background-color: hsl(0deg 0% 23.94%);
+  background-image: url(./assets/pattern.svg);
+  background-size: 18% auto;
+  background-position: center top;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  width: 100%;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  border-radius: 1em;
+  z-index: 999;
+}
+
+.user-info {
+  background-color: var(--dark-blue);
+  padding: 4.5rem 0 1rem 0;
+  border-bottom: 1px solid hsla(0, 0%, 71%, 0.267);
+}
+
+.img-avatar {
+  border-radius: 100px;
+  border: 5px solid var(--dark-blue);
+  position: relative;
+  top: 3rem;
+  z-index: 9999;
+  height: 110px;
+  width: 110px;
+}
+
+.img-avatar:hover {
+  border-color: var(--blue);
+  cursor: pointer;
+  -webkit-transition: all 0.2s ease;
+  -o-transition: all 0.2s ease;
+  transition: all 0.2s ease;
+}
+
+.user-name {
+  font-size: 1.2rem;
+  color: var(--desaturated-blue);
+}
+
+.user-age {
+  font-weight: 400;
+  color: var(--dark-gray);
+}
+
+.user-location {
+  color: var(--dark-gray);
+  font-size: 0.95rem;
+  font-weight: 400;
+}
+
+.user-stats {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: space-evenly;
+  -ms-flex-pack: space-evenly;
+  justify-content: space-evenly;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background-color: var(--dark-blue);
+  padding: 1.15rem;
+  border-radius: 0 0 1em 1em;
+}
+
+.stats-count {
+  font-size: 1.15rem;
+  font-weight: 700;
+  color: var(--desaturated-blue);
+}
+
+.stats-label {
+  font-size: 0.65rem;
+  color: var(--dark-gray);
+  letter-spacing: 1px;
+}
+
+@media (width>=64rem) {
+  .card {
+    width: 21.75rem;
+  }
+
+  .card-banner {
+    height: 8.75rem;
+  }
+
+  .img-avatar {
+    top: 5rem;
+  }
+
+  .user-stats {
+    padding: 1.5rem;
+  }
+}
+</style>
